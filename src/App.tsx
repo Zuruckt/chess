12ce487a7f73c106dedb46cdb762cc7c269f4602
Jeferson Oliveira <zuruckyt@gmail.com>
2023-03-30 22:48:38 -0300

    wip: basic board i guess

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,29 @@
+import classNames from "classnames";
 import "./App.css";
 
 function App() {
+	const x = Array(64).fill(1);
+	let row = 0;
 	return (
-		<div className="App">
-			<header className="App-header">
-				<p>Ramiel</p>
-			</header>
-		</div>
+		<main className="App">
+			<div className="board">
+				{x.map((_, index) => {
+					if ((index + 1) % 8 === 1) {
+						row = row + 1;
+					}
+					let isEven = (index + row) % 2 === 0;
+					return (
+						<div
+							key={`square${index}`}
+							className={classNames("square", {
+								"square-black": !isEven,
+								"square-white": isEven,
+							})}
+						></div>
+					);
+				})}
+			</div>
+		</main>
 	);
 }
 
